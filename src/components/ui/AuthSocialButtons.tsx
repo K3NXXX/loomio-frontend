a5145ca99c10174da25ed8afb642fa20c82a0fb8
K3NXXX <xxvolodyax7@gmail.com>
2025-07-18 14:00:32 +0300

    changed social urls

--- src/components/ui/AuthSocialButtons.tsx
+++ src/components/ui/AuthSocialButtons.tsx
@@ -4,17 +4,26 @@ import { IoLogoGithub } from 'react-icons/io5'
 export function AuthSocialButtons() {
 	return (
 		<div className='flex justify-center gap-5'>
-			<Card
-				onClick={() =>
-					(window.location.href = process.env.NEXT_PUBLIC_GOOGLE_AUTH!)
-				}
-				className='bg-neutral-900 py-5 px-5 cursor-pointer flex justify-center items-center w-full'
+			<a
+				href={process.env.NEXT_PUBLIC_GOOGLE_AUTH!}
+				target='_blank'
+				rel='noopener noreferrer'
+				className='w-full'
 			>
-				<FaGoogle color='#fb2c36' size={20} />
-			</Card>
-			<Card className='bg-neutral-900 py-5 px-5 cursor-pointer flex justify-center items-center w-full'>
-				<IoLogoGithub color='white' size={22} />
-			</Card>
+				<Card className='bg-neutral-900 py-5 px-5 cursor-pointer flex justify-center items-center w-full'>
+					<FaGoogle color='#fb2c36' size={20} />
+				</Card>
+			</a>
+			<a
+				href={process.env.NEXT_PUBLIC_GOOGLE_GITHUB!}
+				target='_blank'
+				rel='noopener noreferrer'
+				className='w-full'
+			>
+				<Card className='bg-neutral-900 py-5 px-5 cursor-pointer flex justify-center items-center w-full'>
+					<IoLogoGithub color='white' size={22} />
+				</Card>
+			</a>
 		</div>
 	)
 }
