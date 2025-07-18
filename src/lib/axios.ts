f80104d6307e31189c7541029a347d6a9d5640ad
K3NXXX <xxvolodyax7@gmail.com>
2025-07-18 23:07:20 +0300

    feat: added env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'https://nextgen-n55f.onrender.com',
+	baseURL: process.env.NEXT_PUBLIC_API_URL,
 	withCredentials: true,
 })
 
