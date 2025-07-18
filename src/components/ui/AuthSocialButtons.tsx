2df088652844c361fed47ba377af741ff33b1c47
idenyy <d3nys.pavlov@gmail.com>
2025-07-18 13:51:55 +0300

    feat: refactor OAuth login handling in AuthSocialButtons component

--- src/components/ui/AuthSocialButtons.tsx
+++ src/components/ui/AuthSocialButtons.tsx
@@ -1,20 +1,37 @@
-import { Card } from '@/components/ui/card'
-import { FaGoogle } from 'react-icons/fa'
-import { IoLogoGithub } from 'react-icons/io5'
+import { Card } from '@/components/ui/card';
+import { FaGoogle } from 'react-icons/fa';
+import { IoLogoGithub } from 'react-icons/io5';
+
 export function AuthSocialButtons() {
+	const handleOAuthLogin = (provider: 'google' | 'github') => {
+		const authUrl =
+			provider === 'google'
+				? process.env.NEXT_PUBLIC_GOOGLE_AUTH!
+				: process.env.NEXT_PUBLIC_GITHUB_AUTH!;
+
+		window.location.href = authUrl;
+	};
+
 	return (
 		<div className='flex justify-center gap-5'>
 			<Card
-				onClick={() =>
-					(window.location.href = process.env.NEXT_PUBLIC_GOOGLE_AUTH!)
-				}
+				onClick={() => handleOAuthLogin('google')}
 				className='bg-neutral-900 py-5 px-5 cursor-pointer flex justify-center items-center w-full'
 			>
-				<FaGoogle color='#fb2c36' size={20} />
+				<FaGoogle
+					color='#fb2c36'
+					size={20}
+				/>
 			</Card>
-			<Card className='bg-neutral-900 py-5 px-5 cursor-pointer flex justify-center items-center w-full'>
-				<IoLogoGithub color='white' size={22} />
+			<Card
+				onClick={() => handleOAuthLogin('github')}
+				className='bg-neutral-900 py-5 px-5 cursor-pointer flex justify-center items-center w-full'
+			>
+				<IoLogoGithub
+					color='white'
+					size={22}
+				/>
 			</Card>
 		</div>
-	)
+	);
 }
